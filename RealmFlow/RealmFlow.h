c36028b19b26f98201ebc7e6ce03ec897f8e9aa4
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2018-02-17 16:31:42 +0900

    Initial Commit

---
+++ RealmFlow/RealmFlow.h
@@ -0,0 +1,19 @@
+//
+//  RealmFlow.h
+//  RealmFlow
+//
+//  Created by jun suzuki on 2018/02/17.
+//  Copyright © 2018年 jun suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmFlow.
+FOUNDATION_EXPORT double RealmFlowVersionNumber;
+
+//! Project version string for RealmFlow.
+FOUNDATION_EXPORT const unsigned char RealmFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmFlow/PublicHeader.h>
+
+
---
+++ RealmFlowTests/RealmFlowTests.swift
@@ -0,0 +1,36 @@
+//
+//  RealmFlowTests.swift
+//  RealmFlowTests
+//
+//  Created by jun suzuki on 2018/02/17.
+//  Copyright © 2018年 jun suzuki. All rights reserved.
+//
+
+import XCTest
+@testable import RealmFlow
+
+class RealmFlowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
