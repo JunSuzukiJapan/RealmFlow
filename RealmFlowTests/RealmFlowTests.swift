89f9f5c4ffc8c2ff037a19417797e905d7534896
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2018-02-17 16:55:11 +0900

    Initial commit.

--- RealmFlowTests/RealmFlowTests.swift
+++ RealmFlowTests/RealmFlowTests.swift
@@ -8,6 +8,15 @@
 
 import XCTest
 @testable import RealmFlow
+import RealmSwift
+
+class Dog : Object {
+    public var name: String = ""
+}
+
+class Cat : Object {
+    public var name: String = ""
+}
 
 class RealmFlowTests: XCTestCase {
     
@@ -21,9 +30,86 @@ class RealmFlowTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAdd(){
+        let poti = Dog()
+        poti.name = "Poti"
+        let tama = Cat()
+        tama.name = "Tama"
+        let hachi = Dog()
+        hachi.name = "Hachi"
+        
+        var flow = Realm.Flow.add(tama)
+            .add(poti)
+        let flow2 = Realm.Flow.add(hachi)
+        flow = flow.combine(flow2)
+        
+        let realm = try! Realm()
+        try? realm.run(flow: flow)
+    }
+    
+    func testQuery(){
+        let flow = Realm.Flow.objects(Dog.self)
+            .subscribe { results in
+                print("results = \(results)")
+                //let a = results.filter(NSPredicate(format: "name != %@", argumentArray: ["Poti"]))
+                for dog in results {
+                    print("1 name: \(dog.name)")
+                }
+            }
+            .filter({ $0.name != "Poti" })
+            .subscribe { results in
+                for dog in results {
+                    print("2 name: \(dog.name)")
+                }
+        }
+        
+        let realm = try! Realm()
+        let _ = try? realm.run(flow: flow)
+    }
+    
+    func testManyFlow() {
+        let pochi = Dog()
+        pochi.name = "Poti"
+        let tama = Cat()
+        tama.name = "Tama"
+        let hachi = Dog()
+        hachi.name = "Hachi"
+        let taro = Dog()
+        taro.name = "Taro"
+        let jiro = Dog()
+        jiro.name = "Jiro"
+        
+        let flow = Realm.Flow.deleteAll()
+            .add(pochi)
+            .add(tama)
+            .add(hachi)
+            .objects(Dog.self)
+            .subscribe { results in
+                print("first subscribe")
+                for dog in results {
+                    print("dog.name: \(dog.name)")
+                }
+            }
+            .objects(Cat.self)
+            .add(taro)
+            .subscribe { results in
+                print("second subscribe")
+                for cat in results {
+                    print("cat.name: \(cat.name)")
+                }
+            }
+            .delete(pochi)
+            .add(jiro)
+            .objects(Dog.self)
+            .subscribe { results in
+                print("third subscribe")
+                for dog in results {
+                    print("dog.name: \(dog.name)")
+                }
+            }
+        
+        let realm = try! Realm()
+        let _ = try? realm.run(flow: flow)
     }
     
     func testPerformanceExample() {
