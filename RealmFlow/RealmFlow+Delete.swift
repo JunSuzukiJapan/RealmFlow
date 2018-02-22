4fe1abd2aa449e016548ebbce9394a623473ad19
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2018-02-22 10:44:41 +0900

    fix: クエリーの後でdelete()が呼ばれた時の処理を変更

--- RealmFlow/RealmFlow+Delete.swift
+++ RealmFlow/RealmFlow+Delete.swift
@@ -9,6 +9,9 @@
 import Foundation
 import RealmSwift
 
+//
+// WriteOnly
+//
 public extension RealmFlow where RW: WriteOnly {
     /// Deletes object in the Realm.
     ///
@@ -21,64 +24,147 @@ public extension RealmFlow where RW: WriteOnly {
         }
     }
     
-    /// Delete all objects in the Realm.
+    /// Deletes queried object in the Realm.
+    /// Always throw an exception
+    ///
+    /// - Parameter object: The object to be removed to this Realm.
+    /// - Returns: `WriteOnly` operation
+    public func delete() throws -> RealmRW<T, Void, NoObject> {
+        throw NSError(domain: "cannot call delete() when no object queried.", code: -1, userInfo: nil)
+    }
+
+    /// Delete all queried objects in the Realm.
+    /// Always throw an exception
     ///
     /// - Returns: `WriteOnly` operation
-    public func deleteAll() -> RealmWO<Void> {
-        return RealmWO<Void> { realm in
-            let _ = try self._run(realm)
-            realm.deleteAll()
-        }
+    public func deleteAll() throws -> RealmWO<Void> {
+        throw NSError(domain: "cannot call deleteAll() when no object queried.", code: -1, userInfo: nil)
     }
 }
 
+//
+// ReadOnly
+//
 public extension RealmFlow where RW: ReadOnly {
     /// Deletes object in the Realm.
     ///
     /// - Parameter object: The object to be removed to this Realm.
     /// - Returns: `ReadWrite` operation
-    public func delete(_ object: Object) -> RealmRW<T, U, ROW> {
-        return RealmRW<T, U, ROW> { realm in
-            let read_result = try self._run(realm)
+    public func delete(_ object: Object) -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let _ = try self._run(realm)
             realm.delete(object)
-            return read_result
-        }
-    }
-    
-    /// Delete all objects in the Realm.
-    ///
-    /// - Returns: `ReadWrite` operation
-    public func deleteAll() -> RealmRW<T, U, ROW> {
-        return RealmRW<T, U, ROW> { realm in
-            let read_result = try self._run(realm)
-            realm.deleteAll()
-            return read_result
         }
     }
 }
 
+/*
+ * This section can not compile by XCode 9.2.
+ *
+public extension RealmFlow where RW: ReadOnly, T: Object, ROW: RawObject {
+    /// Deletes object in the Realm.
+    ///
+    /// - Parameter object: The object to be removed to this Realm.
+    /// - Returns: `ReadWrite` operation
+    public func delete() -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let result: T? = try self._run(realm) as? T
+            if let object = result {
+                realm.delete(object)
+            }
+        }
+    }
+}
+
+
+public extension RealmFlow where RW: ReadOnly, T: Object, U: Results<T>, ROW: RawResults {
+    /// Delete all queried objects in the Realm.
+    ///
+    /// - Returns: `ReadWrite` operation
+    public func deleteAll() -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let results = try self._run(realm)
+            for object in results {
+                realm.delete(object)
+            }
+        }
+    }
+}
+
+public extension RealmFlow where RW: ReadOnly, T: Object, U: SequenceWrapper<T>, ROW: Wrap {
+    /// Delete all queried objects in the Realm.
+    ///
+    /// - Returns: `ReadWrite` operation
+    public func deleteAll() -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let results = try self._run(realm)
+            for object in results {
+                realm.delete(object)
+            }
+        }
+    }
+}
+*/
+
+//
+// ReadWrite
+//
 public extension RealmFlow where RW: ReadWrite {
     /// Deletes object in the Realm.
     ///
     /// - Parameter object: The object to be removed to this Realm.
     /// - Returns: `ReadWrite` operation
-    public func delete(_ object: Object) -> RealmRW<T, U, ROW> {
-        return RealmRW<T, U, ROW> { realm in
-            let read_result = try self._run(realm)
+    public func delete(_ object: Object) -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let _ = try self._run(realm)
             realm.delete(object)
-            return read_result
-        }
-    }
-    
-    /// Delete all objects in the Realm.
-    ///
-    /// - Returns: `ReadWrite` operation
-    public func deleteAll() -> RealmRW<T, U, ROW> {
-        return RealmRW<T, U, ROW> { realm in
-            let read_result = try self._run(realm)
-            realm.deleteAll()
-            return read_result
         }
     }
 }
 
+/*
+ * This section can not compile by XCode 9.2.
+ *
+ public extension RealmFlow where RW: ReadWrite, T: Object, ROW: RawObject {
+    /// Deletes queried object in the Realm.
+    ///
+    /// - Parameter object: The object to be removed to this Realm.
+    /// - Returns: `ReadWrite` operation
+    public func delete() -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let result: T? = try self._run(realm) as? T
+            if let object = result {
+                realm.delete(object)
+            }
+        }
+    }
+}
+
+public extension RealmFlow where RW: ReadWrite, T: Object, U: Results<T>, ROW: RawResults {
+    /// Delete all queried objects in the Realm.
+    ///
+    /// - Returns: `ReadWrite` operation
+    public func deleteAll() -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let results = try self._run(realm)
+            for object in results {
+                realm.delete(object)
+            }
+        }
+    }
+}
+
+public extension RealmFlow where RW: ReadWrite, T: Object, U: SequenceWrapper<T>, ROW: Wrap {
+    /// Delete all queried objects in the Realm.
+    ///
+    /// - Returns: `ReadWrite` operation
+    public func deleteAll() -> RealmRW<T, Void, NoObject> {
+        return RealmRW<T, Void, NoObject> { realm in
+            let results = try self._run(realm)
+            for object in results {
+                realm.delete(object)
+            }
+        }
+    }
+}
+*/
